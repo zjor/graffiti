1f7db2c8437f06d500533638ed3209d75e017bf8
Sergey Royz <zjor.se@gmail.com>
2019-05-16 00:50:14 +0200

    image support

--- js/commons.js
+++ js/commons.js
@@ -13,4 +13,42 @@ export const b64DecodeUnicode = (str) => {
     return decodeURIComponent(atob(str).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-}  
+}
+
+export const encodeText = (text) => {
+    return b64EncodeUnicode('t' + text)
+}
+
+export const encodeImageAndText = (text, imageUrl) => {
+    const obj = {
+        't': text,
+        'i': imageUrl
+    }
+    return b64EncodeUnicode('i' + JSON.stringify(obj))
+}
+
+export const decodeState = (code) => {
+    const decoded = b64DecodeUnicode(code)
+    const state = {}
+    if (decoded[0] == 't') {
+        state.text = decoded.substring(1)
+    } else if (decoded[0] == 'i') {
+        const obj = JSON.parse(decoded.substring(1))
+        state.text = obj.t
+        state.imageUrl = obj.i
+    }
+    return state
+}
+
+export const getMessageElement = (state) => {
+    const textElement = $('<pre/>').addClass('message').text(state.text)
+    if (state.imageUrl !== undefined) {
+        const div = $('<div/>').addClass('center')
+        const img = $('<img/>').addClass('image').attr('src', state.imageUrl)
+        div.append(img)
+        div.append(textElement)
+        return div                
+    } else {
+        return textElement
+    }
+}
